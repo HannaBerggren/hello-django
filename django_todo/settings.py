d5007a71d10275d96b947c93a45e5825407c8583
HannaBerggren <hannajberggren@gmail.com>
2023-03-21 13:32:05 +0000

    removed secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '7htff-o7h2tn^q#lh&98h*$*%z1pmm(7=@$_0-c*grwuykrdjd')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = development
 
 if development:
